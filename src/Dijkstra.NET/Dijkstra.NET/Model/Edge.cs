d4c8b54150807fe12d2f477682c96dc77b5100a4
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2016-10-13 13:04:34 +0200

    implement IEquatable interface

--- src/Dijkstra.NET/Dijkstra.NET/Model/Edge.cs
+++ src/Dijkstra.NET/Dijkstra.NET/Model/Edge.cs
@@ -1,7 +1,8 @@
 ﻿namespace Dijkstra.NET.Model
 {
+    using System;
     using Contract;
-    public struct Edge<T, TCustom> where TCustom: class 
+    public struct Edge<T, TCustom>: IEquatable<Edge<T, TCustom>> where TCustom: class
     {
         public Edge(INode<T, TCustom> node, uint cost, TCustom custom)
         {
@@ -13,5 +14,26 @@
         public INode<T, TCustom> Node { get; }
         public uint Cost { get; }
         public TCustom Item { get; }
+
+        public bool Equals(Edge<T, TCustom> other) => Node.Key == other.Node.Key && Cost == other.Cost && ReferenceEquals(Item, other.Item);
+
+        public override int GetHashCode()
+        {
+            int hash = 13;
+            hash = hash * 7 + (int)Cost;
+            hash = hash * 7 + (int)Node.Key;
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Edge<T, TCustom>?;
+
+            if (other == null)
+                return false;
+
+            return Equals(other.Value);
+        }
+
     }
 }
