1de4719403cd3fd20e5ff9f985f974c759c5ac2d
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2017-12-11 18:43:05 +0100

    implementation

---
+++ src/Samples/Dijkstra.Net40/Program.cs
@@ -0,0 +1,24 @@
+﻿using Dijkstra.NET.Contract;
+using Dijkstra.NET.Model;
+using Dijkstra.NET.ShortestPath;
+
+namespace Dijkstra.Net40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new Graph<int, string>();
+
+            graph.AddNode(1);
+            graph.AddNode(2);
+
+            graph.Connect(0, 1, 5, "some custom information in edge"); //First node has key equal 0
+
+            var dijkstra = new Dijkstra<int, string>(graph);
+            IShortestPathResult result = dijkstra.Process(0, 1); //result contains the shortest path
+
+            var path = result.GetPath();
+        }
+    }
+}
