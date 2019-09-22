e0ad6d09d6971ab52ac9378b3adb92c179a07762
mitchellri <mitchell.rivett@hotmail.com>
2019-09-21 17:07:15 -0700

    AStar add

---
+++ src/Dijkstra.NET/ShortestPath/AStar.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using Dijkstra.NET.Graph;
+
+namespace Dijkstra.NET.ShortestPath
+{
+    internal static class AStar
+    {
+        public static ShortestPathResult GetShortestPath(IDijkstraGraph graph, uint from, uint to, Func<uint,uint,int> heuristic, int depth)
+        {
+            var path = new Dictionary<uint, uint>();
+            var distance = new Dictionary<uint, int> {[from] = 0};
+            var d = new Dictionary<uint, int> {[from] = 0};
+            var q = new SortedSet<uint>(new[] {from}, new NodeComparer(distance, heuristic));
+            var current = new HashSet<uint>();
+
+            int Distance(uint key)
+            {
+                return distance.ContainsKey(key) ? distance[key] : Int32.MaxValue;
+            }
+
+            do
+            {
+                uint u = q.Deque();
+
+                if (u == to)
+                {
+                    return new ShortestPathResult(from, to, distance[u], path);
+                }
+
+                current.Remove(u);
+
+                if (depth == d[u])
+                {
+                    continue;
+                }
+
+                graph[u]((node, cost) =>
+                {
+                    if (Distance(node) > Distance(u) + cost)
+                    {
+                        if (current.Contains(node))
+                        {
+                            q.Remove(node);
+                        }
+
+                        distance[node] = Distance(u) + cost;
+                        q.Add(node);
+                        current.Add(node);
+                        path[node] = u;
+                        d[node] = d[u] + 1;
+                    }
+                });
+
+            } while (q.Count > 0 && depth > 0);
+
+            return new ShortestPathResult(from, to);
+        }
+    }
+}
--- src/Dijkstra.NET/ShortestPath/DijkstraExtensions.cs
+++ src/Dijkstra.NET/ShortestPath/DijkstraExtensions.cs
@@ -23,11 +23,34 @@ namespace Dijkstra.NET.ShortestPath
         /// <param name="graph">Source graph</param>
         /// <param name="from">Start node</param>
         /// <param name="to">End node</param>
+        /// <returns>Value with path</returns>
+        public static ShortestPathResult AStar(this IDijkstraGraph graph, uint from, uint to, Func<uint,uint,int> heuristic)
+            => AStar(graph, from, to, heuristic, Int32.MaxValue);
+
+        /// <summary>
+        /// Get path from @from to @to
+        /// </summary>
+        /// <param name="graph">Source graph</param>
+        /// <param name="from">Start node</param>
+        /// <param name="to">End node</param>
         /// <param name="depth">Depth of path</param>
         /// <returns>Value with path</returns>
         public static ShortestPathResult Dijkstra(this IDijkstraGraph graph, uint from, uint to, int depth)
         {
             return ShortestPath.Dijkstra.GetShortestPath(graph, from, to, depth);
+        }
+        
+        /// <summary>
+        /// Get path from @from to @to
+        /// </summary>
+        /// <param name="graph">Source graph</param>
+        /// <param name="from">Start node</param>
+        /// <param name="to">End node</param>
+        /// <param name="depth">Depth of path</param>
+        /// <returns>Value with path</returns>
+        public static ShortestPathResult AStar(this IDijkstraGraph graph, uint from, uint to, Func<uint, uint, int> heuristic, int depth)
+        {
+            return ShortestPath.AStar.GetShortestPath(graph, from, to, heuristic, depth);
         }
     }
 }
