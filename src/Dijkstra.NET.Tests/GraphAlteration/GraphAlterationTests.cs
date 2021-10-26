4838698206ed346f89d8453697fc58cbabfd55fd
EMEA\dewitr <raf.dewit@dematic.com>
2021-10-26 13:11:48 +0200

    Removed double test

--- src/Dijkstra.NET.Tests/GraphAlteration/GraphAlterationTests.cs
+++ src/Dijkstra.NET.Tests/GraphAlteration/GraphAlterationTests.cs
@@ -30,27 +30,6 @@ namespace Dijkstra.NET.Tests.GraphAlteration
             Assert.Equal(10, result2.Distance);
         }
 
-        [Fact]
-        public void UpdateUnknownConnectionDoesNotWork()
-        {
-            var g = new Graph<int, string>();
-            g.AddNode(1);
-            g.AddNode(2);
-            g.AddNode(3);
-
-            g.Connect(1, 2, 1, "First");
-            g.Connect(2, 3, 2, "Second");
-            g.Connect(1, 3, 10, "Expensive");
-
-            ShortestPathResult result1 = g.Dijkstra(1, 3);
-            Assert.Equal(3, result1.Distance);
-
-            var updateResult = g.UpdateCost(2, 3, "SecondUnknown", 11);
-            Assert.False(updateResult);
-            ShortestPathResult result2 = g.Dijkstra(1, 3);
-            Assert.Equal(3, result2.Distance);
-        }
-
         [Theory]
         [InlineData(2, 5, "Second")]
         [InlineData(5, 3, "Second")]
